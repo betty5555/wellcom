af053b4e319516cbc304749b16679b27cbf8131a
ktm <ktm.britney@gmail.com>
2018-04-04 23:10:04 +0300

    Initial Commit

---
+++ wellcom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wellcom
+//
+//  Created by Mohammed Abdulaziz on ١٨ رجب، ١٤٣٩ هـ.
+//  Copyright © ١٤٣٩ هـ Mohammed Abdulaziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
